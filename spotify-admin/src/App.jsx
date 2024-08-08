99da7f4dc3cd849176435329aa5fc5d307430cdb
Mohd Khaleeq <96943159+Khaleeq01@users.noreply.github.com>
2024-08-08 23:49:08 +0530

    Update App.jsx

--- spotify-admin/src/App.jsx
+++ spotify-admin/src/App.jsx
@@ -9,7 +9,7 @@ import ListAlbum from './pages/ListAlbum/ListAlbum';
 import Sidebar from './components/Sidebar/Sidebar';
 import Navbar from './components/Navbar/Navbar';
 
-export const url = 'http://localhost:4000';
+export const url = 'https://spotify-clone-backend-2bp7.onrender.com/';
 
 const App = () => {
   return (
